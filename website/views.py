cba4ae234978477be9f2eeb066f345a3701a7600
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-30 06:11:09 +0330

    website views - handling contact form submission
    Added contact form handling to our contact view also adding a message to tell the user about form submission

--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from website.forms import ContactForm
+from django.contrib import messages
 
 
 def index_view(request):
@@ -17,7 +20,16 @@ def index_view(request):
 
 
 def contact_view(request):
-    return render(request, "website/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Your Ticket Submitted Successfully!")
+        else:
+            messages.add_message(request, messages.ERROR, "Your Ticket Didn't Submit! Please try again.")
+        return HttpResponseRedirect('/')
+    form = ContactForm()
+    return render(request, "website/contact.html", {"form": form})
 
 
 def about_view(request):
