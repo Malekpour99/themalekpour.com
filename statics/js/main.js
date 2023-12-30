bd714ed4343165f34b25986c631c57b154323b77
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-30 05:49:00 +0330

    style:main.js
    Removed some unnecessary lines

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,9 +1,6 @@
 /**
 * Template Name: FlexStart
 * Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
 */
 (function() {
   "use strict";
