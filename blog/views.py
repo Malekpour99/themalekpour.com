06a440066220508c0d79b1cb389455b968c57314
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-01 05:58:18 +0330

    blog views - blog & blog-single views
    Created blog and blog-single view in order to render these templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, "blog/blog.html")
+
+
+def single_view(request):
+    return render(request, "blog/blog-single.html")
