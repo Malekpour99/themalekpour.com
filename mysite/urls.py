33870c926a2c4e7d2442477e00c5c389c0c4fc71
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-01 05:34:02 +0330

    mysite URLs - blog app URL
    Added blog app URL to our project URLs

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("website.urls")),
+    path("blog/", include("blog.urls")),
     path('captcha/', include('captcha.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
