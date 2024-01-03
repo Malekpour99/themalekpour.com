7cd2f44ec0edacab57ac4e20cd148d37eacb6fc7
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-03 09:00:28 +0330

    blog admin - post model registeration
    Registered post model in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    # Activates post filtering by date hierarchy
+    empty_value_display = "-empty-"
+    # Determines how empty values are displayed
+    list_display = (
+        "title",
+        "author",
+        "counted_views",
+        "status",
+        "require_login",
+        "published_date",
+        "created_date",
+    )
+    list_filter = ("status", "author")
+    # + "fields" can also be used to determine
+    #   which fields to display on each data-editing page
+    # ordering = ['-created_date'] moved to Meta class
+    # Sets the ordering of your data list
+    search_fields = ["title", "content"]
+    # Determines which fields will be searched for your search query
