c58c3211a98bd1515b0fe1c19843d0859b6d56f0
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-03 08:59:00 +0330

    blog models - created Post model
    Created post model in order to add, edit and publish posts in our blog and manage them

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tags = TaggableManager()
+    # category = models.ManyToManyField(Category)
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField()
+    require_login = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_date"]
+        # ordering applied here is general, not just limited to the admin page
+        # you can change your presented class name
+        #   using "verbose_name" and "verbose_name_plural"
+
+    def __str__(self):
+        return f" {self.title} - {self.id}"
+
+    def get_absolute_url(self):
+        return reverse("blog:single", kwargs={"pid": self.id})
