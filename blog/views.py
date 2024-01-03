f6037513b212b35204fbfa36cfbe9bfd7852c72b
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-03 10:16:38 +0330

    blog views - home view posts
    Now posts are fetched from the database and will be injected to the template using context object

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 
 def home_view(request):
-    return render(request, "blog/blog.html")
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now(), status=True)
+    context = {'posts': posts}
+    return render(request, "blog/blog.html", context)
 
 
 def single_view(request):
