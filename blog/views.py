fe6c36e92ce2264cd5b528f46dc096f646bd3734
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-03 18:15:49 +0330

    blog views - single_view post's ID
    Updated single_view to fetch the related post from database in order to show its content and related information if that post has been published

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -10,5 +10,16 @@ def home_view(request):
     return render(request, "blog/blog.html", context)
 
 
-def single_view(request):
-    return render(request, "blog/blog-single.html")
+def single_view(request, pid):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now(), status=True)
+    post = get_object_or_404(posts, id=pid)
+    # checking if the post requires user to be logged in
+    if not post.require_login or request.user.is_authenticated:
+        # post = get_object_or_404(Post, id=pid) # this is unsafe because you can access not published posts by using ID
+        post.counted_views += 1
+        post.save()
+        context = {'post': post, }
+        return render(request, "blog/blog-single.html", context)
+    else:
+        return render(request, "blog/blog-single.html", context)
