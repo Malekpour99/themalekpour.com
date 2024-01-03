47fdbc3775b61f345f39db0b44e9548a2618b269
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-03 09:24:41 +0330

    blog models - created Category model
    Created category models for assigning different categories to different posts and managing posts based on their categories

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
