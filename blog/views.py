a1c150e279736c0154f1e51febbef326e018e652
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-05 08:34:52 +0330

    blog views - search view
    Added search view for searching blog posts based on their content or title

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
+from django.db.models import Q
 
 
 def home_view(request):
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now(), status=True)
-    context = {'posts': posts}
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=True)
+    context = {"posts": posts}
     return render(request, "blog/blog.html", context)
 
 
 def single_view(request, pid):
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now(), status=True)
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=True)
     post = get_object_or_404(posts, id=pid)
     # checking if the post requires user to be logged in
     if not post.require_login or request.user.is_authenticated:
@@ -20,7 +19,19 @@ def single_view(request, pid):
         post.counted_views += 1
         post.save()
         tags = post.tags.all()
-        context = {'post': post, 'tags': tags }
+        context = {"post": post, "tags": tags}
         return render(request, "blog/blog-single.html", context)
     else:
         return render(request, "blog/blog-single.html", context)
+
+
+def search_view(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=True)
+    if request.method == "GET":
+        # python "warlus" option ( := ) assign the value to the variable -> less code
+        if search_query := request.GET.get("search"):
+            posts = posts.filter(
+                Q(content__contains=search_query) | Q(title__contains=search_query)
+            )
+    context = {"posts": posts}
+    return render(request, "blog/blog.html", context)
