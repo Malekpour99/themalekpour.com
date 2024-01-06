0ee22e7b999794cae0e10ba54e990ee4789b1ba7
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-06 18:25:58 +0330

    blog views - single_view post comment
    Retrieving each post comments from database and sending them to the template through context

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from blog.models import Post
+from blog.models import Post, Comment
 from blog.forms import CommentForm
 from django.db.models import Q
 from django.contrib import messages
@@ -20,23 +20,30 @@ def home_view(request, **kwargs):
 
 
 def single_view(request, pid):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.add_message(request, messages.SUCCESS, "Your Comment Submitted Successfully!")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Your Comment Submitted Successfully. Thanks!",
+            )
         else:
-            messages.add_message(request, messages.ERROR, "Your Comment Didn't Submit!")
+            messages.add_message(
+                request, messages.ERROR, "Your Comment Didn't Submit! Please try again."
+            )
 
     posts = Post.objects.filter(published_date__lte=timezone.now(), status=True)
     post = get_object_or_404(posts, id=pid)
     # checking if the post requires user to be logged in
     if not post.require_login or request.user.is_authenticated:
+        comments = Comment.objects.filter(post=post, approved=True)
         tags = post.tags.all()
         form = CommentForm()
         post.counted_views += 1
         post.save()
-        context = {"post": post, "tags": tags, "form": form}
+        context = {"post": post, "tags": tags, "form": form, "comments": comments}
         return render(request, "blog/blog-single.html", context)
     else:
         return render(request, "blog/blog-single.html", context)
@@ -50,6 +57,6 @@ def search_view(request):
             posts = posts.filter(
                 Q(content__contains=search_query) | Q(title__contains=search_query)
             )
-            page_title = "Searched for: " + "\"" + search_query + "\""
+            page_title = "Searched for: " + '"' + search_query + '"'
     context = {"posts": posts, "page_title": page_title}
     return render(request, "blog/blog.html", context)
