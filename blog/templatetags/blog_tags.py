a60481c6beab0914a0c9b4d1ab3a8e3d4090c7ec
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-06 05:10:12 +0330

    blog_tags - recent posts inclusion custom tag
    Added recent posts inclusion custom tag to our blog tags for retrieving latest posts from our database in show them in our blog-recent-posts component

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,14 +1,17 @@
 from django import template
 from blog.models import Post, Category
 from taggit.models import Tag
+from django.utils import timezone
 
 register = template.Library()
+
 
 @register.inclusion_tag("blog/includes/blog-tags.html")
 def all_tags():
     "Retrieving all of the tags"
     tags = Tag.objects.all()
     return {"tags": tags}
+
 
 @register.inclusion_tag("blog/includes/blog-categories.html")
 def post_categories():
@@ -19,3 +22,12 @@ def post_categories():
     for category in categories:
         cat_dict[category] = posts.filter(category=category).count()
     return {"categories": cat_dict}
+
+
+@register.inclusion_tag("blog/includes/blog-recent-posts.html")
+def recent_posts(num=5):
+    "Retrieving latest posts"
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by(
+        "-published_date"
+    )[:num]
+    return {"posts": posts}
