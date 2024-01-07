6bcc784d72bdf88b694fb61b6628194cf3997d9c
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-07 05:36:55 +0330

    blog URLs - author URL
    Added author URL path to the blog URLs for filtering posts based on their author using this URL path

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<int:pid>", single_view, name="single"),
     path("category/<str:cat_name>", home_view, name="category"),
     path("tag/<str:tag_name>", home_view, name="tag"),
+    path("author/<str:author_username>", home_view, name="author"),
     path("search/", search_view, name="search"),
 ]
