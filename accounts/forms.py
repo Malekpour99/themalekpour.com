a51d4fb184652f2236e665ab03be361225ced161
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-01-09 19:08:50 +0330

    accounts forms - custom user creation form
    Created a custom-user-creation-form in order to manage new user creations and prevent using duplicate email addresses for signing up

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username_or_email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        # Check if the email is already associated with an existing user
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address is already in use!")
+
+        return email
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
